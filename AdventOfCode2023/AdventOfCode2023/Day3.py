d986e5d364b235140730112bd5adbeab589be1d0
Adam Carr <acarr@jhu.edu>
2023-12-03 22:02:46 -0500

    changed dictionary strategy

--- AdventOfCode2023/AdventOfCode2023/Day3.py
+++ AdventOfCode2023/AdventOfCode2023/Day3.py
@@ -1,39 +1,24 @@
-file = open('inputs/test.txt')
+import re
+
+file = open('inputs/day3.txt')
 lines = file.readlines()
 
 #part1
 partNumSum = 0
-partNumSet = {}
+partNumSet = set()
 
-#build a dictionary of keys to dictionary. Number of keys should match the number of lines
-coordDict = {
-    0:{0:[]}
-}
+#build a dictionary of parts and their coordinate data by row number
+partDict = {}
 
-idx = 0
+lineNum = 0
 
-while idx < len(lines):
-    coordDict[idx] = {0:{}}
-    idx += 1
+while lineNum < len(lines):
+    partDict[lineNum] = {}
+    lineNum += 1
     
 print('done building parent dict')
 
-#for each key, populate it's dictionary with a key to empty set. Keys size should match the length of a line in lines
-lineIdx = 0
-
-for key in coordDict.keys():
-    idx = 0
-    
-    while idx < len(lines[lineIdx]):
-        coordDict[key][idx]={}
-        idx += 1
-        
-    lineIdx += 1
-
-
-print('done nesting child dicts')
-
-#find part numbers in each line and map them into the coordDict
+#for each add a key for each part with a list value containing all it's index locations
 lineNum = 0
 while lineNum < len(lines):
     line = lines[lineNum]
@@ -43,19 +28,59 @@ while lineNum < len(lines):
     idx = 0
     while idx < len(line):
         char = line[idx]
-        coordList.append(idx)
-        if (not char.isdigit):
+        if(not re.match('[0-9]',char)):
             if(tempStr):
-                for coord in coordList:
-                    coordDict[lineNum][coord].append(int(tempStr))
-                tempStr = ''
+                partDict[lineNum][int(tempStr)] = coordList
+            tempStr = ''
+            coordList = []
+            
         else:
             tempStr += char
+            coordList.append(idx)
         idx += 1
     
     lineNum += 1
 
-print('done mapping part numbers to coord map')
+print('done populating partDict')
+
+
+def mark_part_numbers(lidx, idx):
+
+    for partNumber in partDict[lidx].keys():
+        coords = partDict[lidx][partNumber]
+        if idx - 1 in coords or idx in coords or idx +1 in coords:
+            coords.append('isPart')
+            
+    return
+
+
+#go through lines looking for special characters and use their indexes to find part numbers
+lineNum = 0
+prevSpecialIndexes = []
+while lineNum < len(lines):
+    line = lines[lineNum]
+    idx = 0
+    
+    while idx < len(line):
+        if(not re.match('[0-9\.]',line[idx])):
+            if(lineNum > 0):
+                #mark real part numbers on the line above current line
+                mark_part_numbers(lineNum - 1, idx)
+            #mark real part numbers on the current line
+            mark_part_numbers(lineNum, idx)
+            if(lineNum < len(lines) - 1):
+                #mark real part numbers on the line below current line
+                mark_part_numbers(lineNum + 1, idx)
+        idx += 1
+    
+    lineNum += 1
+
+for rowNum in partDict.keys():
+    for partNumber in partDict[rowNum].keys():
+        if 'isPart' in partDict[rowNum][partNumber]:
+            partNumSum += partNumber
+
+print("The sum of all part numbers is " + str(partNumSum))
                 
             
 
